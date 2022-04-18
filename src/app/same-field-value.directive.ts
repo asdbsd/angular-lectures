48f894680451a6ac924f70edbd5400bb444245d7
Dobriyan Petkov <dobriyanp@yahoo.com>
2022-04-18 01:09:42 +0100

    completed template driven forms

--- src/app/same-field-value.directive.ts
+++ src/app/same-field-value.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, OnDestroy} from '@angular/core';
 import { AbstractControl, NgForm, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appSameFieldValue]',
@@ -11,22 +12,44 @@ import { AbstractControl, NgForm, NG_VALIDATORS, ValidationErrors, Validator } f
     }
   ]
 })
-export class SameFieldValueDirective implements Validator {
+export class SameFieldValueDirective implements OnDestroy, Validator {
 
   @Input() appSameValue: string = '';
   @Input() name!: string;
 
+  otherControl!: AbstractControl;
+  subscription!: Subscription;
+
   constructor(private form: NgForm) {
   }
 
+  // ngOnChanges(simpleChanges: SimpleChanges): void {
+  //   if (simpleChanges['appSameValue']) {
+  //     if (this.subscription) { this.subscription.unsubscribe(); }
+  //     this.otherControl = this.form.controls['password'];
+  //     this.subscription = this.control.valueChanges?.subscribe(() => {
+  //       // this.control updateValueAndValidity({ onlySelf: true });
+  //     })
+  //   }
+  // }
+
   validate(control: AbstractControl): ValidationErrors | null {
-    const otherFieldValue = this.form.controls['password']?.value;
-    return control.value !== otherFieldValue ? {
+    const otherControl = this.form.controls['password'];
+    if(this.subscription) { this.subscription.unsubscribe(); };
+    this.subscription = otherControl.valueChanges.subscribe(() => {
+      control.updateValueAndValidity({ onlySelf: true });
+    });
+
+    return control.value !== otherControl.value ? {
       sameValue: {
-        [this.appSameValue]: otherFieldValue,
-        [this.name] : control.value
+        [this.appSameValue]: otherControl.value,
+        [this.name]: control.value
       }
     } : null
   } 
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
